6a3f44b9a932b17f1c7e04f61726a95d36bf103a
Tom Vermeeren <r0832730@student.thomasmore.be>
2022-10-26 10:10:32 +0200

    main.dart terug werkend maken

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,15 @@ void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  static const String _title = 'Verkeersborden app Jan & Tom';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: _title,
+      home: MyStatefulWidget(),
     );
   }
 }
