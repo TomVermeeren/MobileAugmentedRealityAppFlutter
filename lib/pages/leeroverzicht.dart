e55d9ec43c10d5d694713184bf62ca0bc7b6f933
jvanhove <r0842952@student.thomasmore.be>
2022-10-26 21:22:11 +0200

    eerste versie camera

---
+++ lib/pages/leeroverzicht.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'leermenu.dart';
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const LeerOverzicht(),
+      ),
+    );
+  }
+}
+
+class LeerOverzicht extends StatelessWidget {
+  const LeerOverzicht({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Card(
+              child: ElevatedButton(
+                onPressed: () {
+                  Card(){
+                    
+                  }
+                },
+                child: const SizedBox(
+                  width: 300,
+                  height: 100,
+                  child: Text('A card that can be tapped'),
+                ),
+              ),
+            ),
+            Card(
+              child: ElevatedButton(
+                onPressed: () {
+                  Card(){
+                    
+                  }
+                },
+                child: const SizedBox(
+                  width: 300,
+                  height: 100,
+                  child: Text('A card that can be tapped'),
+                ),
+              ),
+            ),]
+      ),
+    );
+  }
+}
