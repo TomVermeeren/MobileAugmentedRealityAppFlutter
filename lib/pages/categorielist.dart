ccb51fc41b6f886cd40e3f73af68c935fcb47606
jvanhove <r0842952@student.thomasmore.be>
2022-10-27 16:57:21 +0200

    Categorie pagine is basically af, voor mobile toch

--- lib/pages/categorielist.dart
+++ lib/pages/categorielist.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import '../models/verkeersbord.dart';
 import '../models/categorie.dart';
 import '../apis/verkeersbord_api.dart';
@@ -16,9 +17,6 @@ class _CategorieListPageState extends State {
   int count = 0;
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Categorieen"),
-      ),
       body: Container(
         padding: const EdgeInsets.all(5.0),
         child: categorieListItems(),
@@ -43,24 +41,102 @@ class _CategorieListPageState extends State {
 
   ListView categorieListItems() {
     return ListView.builder(
-      itemCount: count,
-      itemBuilder: (BuildContext context, int position) {
-        return Card(
-          color: Colors.white,
-          elevation: 2.0,
-          child: ElevatedButton(
-            child: Text(categorieList[position].naam),
-            onPressed: () {
-              Navigator.push(
-                context,
-                new MaterialPageRoute(
-                    builder: (context) =>
-                        MyRoute(titel: categorieList[position].naam)),
-              );
-            },
-          ),
-        );
-      },
-    );
+        itemCount: count,
+        itemBuilder: (BuildContext context, int position) {
+          AssetImage afbeelding1 =
+              AssetImage(categorieList[position].afbeeldingLink);
+
+          return Container(
+              margin: const EdgeInsets.all(5.0),
+              child: (() {
+                if (categorieList[position].id.isOdd &&
+                    categorieList[position].id <= categorieList.length - 1) {
+                  AssetImage afbeelding2 =
+                      AssetImage(categorieList[position + 1].afbeeldingLink);
+                  return Row(children: [
+                    Expanded(
+                      child: Card(
+                        color: Colors.white,
+                        elevation: 2.0,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                new MaterialPageRoute(
+                                    builder: (context) => MyRoute(
+                                        titel: categorieList[position].naam)),
+                              );
+                            },
+                            child: Column(children: [
+                              Container(
+                                  margin: EdgeInsets.all(20),
+                                  child: Image(
+                                    height: 150,
+                                    image: afbeelding1,
+                                  )),
+                              Text(categorieList[position].naam),
+                            ])),
+                      ),
+                    ),
+                    Expanded(
+                      child: Card(
+                          color: Colors.white,
+                          elevation: 2.0,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  new MaterialPageRoute(
+                                      builder: (context) => MyRoute(
+                                          titel: categorieList[position + 1]
+                                              .naam)),
+                                );
+                              },
+                              child: Column(children: [
+                                Container(
+                                    margin: EdgeInsets.all(20),
+                                    child: Image(
+                                      height: 150,
+                                      image: afbeelding2,
+                                    )),
+                                Text(categorieList[position + 1].naam),
+                              ]))),
+                    )
+                  ]);
+                } else if (categorieList[position].id.isOdd) {
+                  return Row(children: [
+                    Expanded(
+                        child: Card(
+                            semanticContainer: true,
+                            color: Colors.white,
+                            elevation: 5,
+                            clipBehavior: Clip.antiAliasWithSaveLayer,
+                            child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    new MaterialPageRoute(
+                                        builder: (context) => MyRoute(
+                                            titel:
+                                                categorieList[position].naam)),
+                                  );
+                                },
+                                child: Column(children: [
+                                  Container(
+                                    margin: EdgeInsets.all(20),
+                                    child: Image(
+                                      height: 150,
+                                      image: afbeelding1,
+                                    ),
+                                  ),
+                                  Text(categorieList[position].naam),
+                                ])))),
+                    Expanded(
+                      child: Text(""),
+                    )
+                  ]);
+                }
+              }()));
+        });
   }
 }
