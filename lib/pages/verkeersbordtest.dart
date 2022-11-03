7bf445050bf361ab4622b2dc7187aff786f5a596
jvanhove <r0842952@student.thomasmore.be>
2022-11-03 21:12:08 +0100

    kleine punten update

--- lib/pages/verkeersbordtest.dart
+++ lib/pages/verkeersbordtest.dart
@@ -23,7 +23,7 @@ class _DetailPageState extends State {
   int nextposition = 0;
   String alertButtonText = "";
   String foutbericht = "";
-
+  List<int> values = [];
   int punten = 0;
   Random random = new Random();
   @override
@@ -40,6 +40,27 @@ class _DetailPageState extends State {
   void initState() {
     super.initState();
     _setMessage("Default", alertButtonText);
+    punten = 0;
+    for (Verkeersbord bord in list) {
+      if (bord.gehaald == "1") {
+        punten++;
+      }
+    }
+    // Hier stellen we de posities van de foute antwoorden vast, deze mogen niet gelijk zijn aan elkaar of aan het juiste antwoord
+    var max = 0;
+
+    if (list.length == 0) {
+      max = 1;
+    } else {
+      max = list.length - 1;
+    }
+    int nextvalue = 0;
+    for (int i = 0; i < 3; i++) {
+      do {
+        nextvalue = random.nextInt(max);
+      } while (values.contains(nextvalue) || nextvalue == position);
+      values.add(nextvalue);
+    }
   }
 
   void _setFoutbericht(String foutbericht) {
@@ -58,6 +79,12 @@ class _DetailPageState extends State {
           });
         }
       });
+      punten = 0;
+      for (Verkeersbord bord in list) {
+        if (bord.gehaald == "1") {
+          punten++;
+        }
+      }
       this.alertButtonText = alertButtonText;
     });
   }
@@ -71,8 +98,6 @@ class _DetailPageState extends State {
       message = "Correct!";
       alertButtonText = "Volgende vraag";
       list[position].gehaald = "1";
-      VerkeersbordApi.updateVerkeersbord(list[position].id, list[position])
-          .then((result) {});
 
       while ((nextposition == position ||
               list[nextposition].categorie != currentCategorie) &&
@@ -99,29 +124,19 @@ class _DetailPageState extends State {
         builder: (BuildContext context) => _buildPopupDialog(context),
       );
     } else {
+      list[position].gehaald = "0";
       foutbericht = "Fout! Probeer opnieuw.";
       _setFoutbericht(foutbericht);
+      _setMessage(message, alertButtonText);
     }
+    VerkeersbordApi.updateVerkeersbord(list[position].id, list[position])
+        .then((result) {});
   }
 
   Scaffold toonAntwoorden() {
     // Positie bepalen van volgend verkeersbord binnen deze categorie
-    var max = 0;
 
-    if (list.length == 0) {
-      max = 1;
-    } else {
-      max = list.length - 1;
-    }
     // Dit legt de posities van de foute antwoorden vast, deze mogen niet gelijk zijn aan elkaar of aan het juiste antwoord
-    List<int> values = [];
-    int nextvalue = 0;
-    for (int i = 0; i < 3; i++) {
-      do {
-        nextvalue = random.nextInt(max);
-      } while (values.contains(nextvalue) || nextvalue == position);
-      values.add(nextvalue);
-    }
 
     AssetImage avatarAsset = AssetImage(list[position].afbeeldingLink);
     return Scaffold(
@@ -187,7 +202,7 @@ class _DetailPageState extends State {
                     },
                     child: Text(list[values[2]].naam),
                   ),
-                  Text(foutbericht + list[position].gehaald)
+                  Text(foutbericht + punten.toString())
                 ],
               )
             ],
@@ -217,6 +232,7 @@ class _DetailPageState extends State {
               Navigator.push(context,
                   new MaterialPageRoute(builder: (context) => RouteToHome()));
             } else {
+              _setMessage(message, alertButtonText);
               Navigator.push(
                   context,
                   new MaterialPageRoute(
